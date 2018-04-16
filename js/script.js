36f787d6cf09a32b5c83d51f1f274fda1d5373bb
shradhakatyal <skatyal2204@gmail.com>
2018-04-16 22:07:26 +0530

    reset feature added

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var isOpen = false;
 var firstClick = true;
 var temp = [];
 var sec;
+var i;
 var bgColors = [];
 var copyArray = [];
 var timer;
@@ -40,7 +41,9 @@ var colorArray = [{
     'count': 2
 }];
 
-copyArray = [].concat(colorArray);
+// To create a deep copy of colorArray.
+
+copyArray = JSON.parse(JSON.stringify(colorArray));
 
 // Assigning IDs to all the tiles
 
@@ -51,21 +54,26 @@ $('.tile').each(function (index) {
 $('.inner-tile').each(function (index) {
     $(this).attr('id', 'inner-tile-' + index);
     $(this).css('display', 'none');
-    randomColor();
+    randomColor()
 });
 
-//To reset game
+//Clicking the reset button calls a method that resets the game
 
 $('#reset').on('click', function () {
     resetGame();
 });
 
+// Method to reset the game.
+
 function resetGame() {
     clearInterval(timer);
     $('#timer').text('0');
-    copyArray = [].concat(colorArray);
+    copyArray = JSON.parse(JSON.stringify(colorArray));
+    console.log(copyArray);
     totalMoves = 0;
+    numMatches = 0;
     isOpen = false;
+    firstClick = true;
     temp = [];
     bgColors = [];
     updateMovesCounter();
@@ -83,12 +91,17 @@ function resetGame() {
 function randomColor() {
     randomColorGenerator();
     cleanArray();
-    $('.inner-tile').each(function (i) {
+    i = 0;
+    $('.inner-tile').each(function () {
         $(this).css('backgroundColor', bgColors[i]);
+        i++;
     });
 }
 
+// Function that generates an array of random colors.
+
 function randomColorGenerator() {
+    console.log(copyArray);
     var index = Math.floor(Math.random() * copyArray.length);
     var lastIndex = copyArray.length - 1;
     if (copyArray[index].count > 0) {
@@ -104,6 +117,7 @@ function randomColorGenerator() {
     }
 }
 
+// Function to clean the array of random colors, filters out falsy values.
 
 function cleanArray() {
     bgColors = bgColors.filter(function (val) {
@@ -113,8 +127,10 @@ function cleanArray() {
     });
 }
 
+// Function to update the star rating as per number of moves
+
 function updateStarRating() {
-    if (totalMoves > 6) {
+    if (totalMoves > 16) {
         $('#star-3').fadeOut('slow');
     }
     if (totalMoves > 24) {
@@ -124,6 +140,7 @@ function updateStarRating() {
         $('#star-1').fadeOut('slow');
     }
 }
+// Function to start timer at the start of each game.
 
 function startTimer() {
     sec = 0;
@@ -132,23 +149,27 @@ function startTimer() {
     }, 1000);
 }
 
+// Function to update the display of moves counter.
 
 function updateMovesCounter() {
     $('#counter').text(totalMoves);
 }
 
-$('#no').on('click', function() {
+// Functions for further action depending on whether the user clicks yes or no.
+
+$('#no').on('click', function () {
     $('.modal').css('display', 'none');
     $('body').css('overflow', 'auto');
     alert('Sorry to see you go..')
 });
 
 
-$('#yes').on('click', function() {
+$('#yes').on('click', function () {
     $('.modal').css('display', 'none');
     $('body').css('overflow', 'auto');
     resetGame();
 })
+
 // on click function for the tiles
 
 $('.tile').on('click', function () {
@@ -166,12 +187,7 @@ $('.tile').on('click', function () {
         if ($('#' + temp[0]).children().css('backgroundColor') === $('#' + temp[1]).children().css('backgroundColor')) {
             numMatches++;
             if (numMatches === 8) {
-                window.scrollTo(0, 0);
-                $('#rating').addClass('fa', 'fa-star');
-                $('#time').text(sec);
-                $('.modal').css('display', 'block');
-                $('body').css('overflow', 'hidden');
-                clearInterval(timer);
+                gameWon();
             }
             else {
                 $('#' + temp[1]).click(false);
@@ -199,3 +215,26 @@ $('#close-modal').on('click', function () {
     $('body').css('overflow', 'auto');
     resetGame();
 });
+
+// Gets called when the user wins the game
+// Displays the game won modal
+
+function gameWon() {
+    window.scrollTo(0, 0);
+    if (totalMoves <= 16) {
+        $('#rating1').addClass('fa-star fa');
+        $('#rating2').addClass('fa-star fa');
+        $('#rating3').addClass('fa-star fa');
+    }
+    else if (totalMoves > 16 && totalMoves <= 24) {
+        $('#rating1').addClass('fa-star fa');
+        $('#rating2').addClass('fa-star fa');
+    }
+    else if (totalMoves > 24) {
+        $('#rating1').addClass('fa-star fa');
+    }
+    $('#time').text(sec);
+    $('.modal').css('display', 'block');
+    $('body').css('overflow', 'hidden');
+    clearInterval(timer);
+}
